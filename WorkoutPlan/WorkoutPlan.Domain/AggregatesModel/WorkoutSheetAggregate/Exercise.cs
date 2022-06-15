4d8485aa4f19f3549d93c9d510220e0f2feb97d6
Rogerson Nazário <rrnazario@gmail.com>
2022-06-15 22:03:59 +0200

    Evolving Exercise

--- WorkoutPlan/WorkoutPlan.Domain/AggregatesModel/WorkoutSheetAggregate/Exercise.cs
+++ WorkoutPlan/WorkoutPlan.Domain/AggregatesModel/WorkoutSheetAggregate/Exercise.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using WorkoutPlan.Domain.SeedWork;
 
 namespace WorkoutPlan.Domain.AggregatesModel.WorkoutSheetAggregate
 {
-    public record Exercise
+    public record Exercise 
+        : Entity
     {
+        public string Name { get; }
+        public string Description { get; }
+        
+        private List<string> _medias;
+        public IReadOnlyCollection<string> Medias => _medias.AsReadOnly();
+
+        public Exercise(string name, string description, List<string> medias)
+        {
+            Name = name;
+            Description = description;
+            _medias = medias;
+        }
     }
 }
